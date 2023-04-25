9f206dcdad948170b2c266b727109de24e3fc24b
shasherazi <hassanrandomz@gmail.com>
2023-04-25 13:59:01 +0500

    Add movie class

---
+++ classes/movie.rb
@@ -0,0 +1,13 @@
+class Movie < Item
+  def initialize(silent, publish_date)
+    super(publish_date)
+    @silent = silent
+    @items = []
+  end
+
+  def can_be_archived?
+    # movie can be archived if parent method returns true
+    # or if it is silent
+    super || @silent
+  end
+end
