a53d866f14558c1e6701c3c7bbcf007ade9d50d7
Fene-87 <fenekayas87@gmail.com>
2023-04-25 12:05:19 +0300

    Create Game class

---
+++ classes/game.rb
@@ -0,0 +1,11 @@
+class Game < Item
+    def initialize(multiplayer, last_played_at)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+
+    private
+
+    def can_be_archived?
+    end
+end
