ead7e39323865392dc36f9ccea393d5905dee642
MAleemH <aleemhashmi4321@gmail.com>
2023-04-26 17:47:17 +0500

    Fixed linter errrors

--- classes/book.rb
+++ classes/book.rb
@@ -1,17 +1,16 @@
 class Book
-    attr_accessor :publisher, :cover_state
-  
-    def initialize(publisher, cover_state)
-      @publisher = publisher
-      @cover_state = cover_state
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    if @cover_state == "torn" || @cover_state == "worn"
+      return false
+    else
+      return true
     end
-  
-    def can_be_archived?
-      if @cover_state == "torn" || @cover_state == "worn"
-        return false
-      else
-        return true
-      end
-    end
+  end
 end
-  
