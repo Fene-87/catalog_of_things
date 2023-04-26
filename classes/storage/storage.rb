d8226796087cc0837f1b73ffc4220271d090a92f
shasherazi <hassanrandomz@gmail.com>
2023-04-26 13:27:26 +0500

    Create parent storage class

---
+++ classes/storage/storage.rb
@@ -0,0 +1,17 @@
+class Storage
+  def fetch
+    raise NotImplementedError
+  end
+
+  def store(_item)
+    raise NotImplementedError
+  end
+
+  def serialize(_item)
+    raise NotImplementedError
+  end
+
+  def deserialize(_item)
+    raise NotImplementedError
+  end
+end
