e42211e2504bf9272e297a41358d7d6b6155ae3d
MAleemH <aleemhashmi4321@gmail.com>
2023-04-27 16:46:56 +0500

    Implemented can_be_archived? method

--- classes/book.rb
+++ classes/book.rb
@@ -1,14 +1,19 @@
-class Book
+require './item'
+
+class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state)
+    super()
     @publisher = publisher
     @cover_state = cover_state
   end
 
   def can_be_archived?
-    return false if @cover_state == 'torn' || @cover_state == 'worn'
-
-    true
+    if super || cover_state == "bad"
+      return true
+    else
+      return false
+    end
   end
 end
